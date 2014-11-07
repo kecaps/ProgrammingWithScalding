89712018c0eaf9c886e74d62a253e2b6b95fea2f
Space <space@wibidata.com>
2014-11-06 22:01:33 -0800

    fixed some chapter 3 shell scripts

--- chapter3/createJan2014.sh
+++ chapter3/createJan2014.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
+. $(dirname $0)/../hdfs-root-env.sh
+
 # Creates a local file called YYYYmmDD and puts it on HDFS folder /dataset/2014/01/xx/YYYYmmDD for Jan 2014
-hadoop fs -rm -r /dataset
+hadoop fs -rm -r ${HDFS_ROOT}/dataset
 for i in 0{1..9} {10..31} ; do
-    hadoop fs -mkdir -p /dataset/2014/01/$i/
+    hadoop fs -mkdir -p ${HDFS_ROOT}/dataset/2014/01/$i/
     echo "2014-01-$i" > 201401$i
-    hadoop fs -put 201401$i /dataset/2014/01/$i/
+    hadoop fs -put 201401$i ${HDFS_ROOT}/dataset/2014/01/$i/
 done
--- chapter3/runHdfsInputTest.sh
+++ chapter3/runHdfsInputTest.sh
@@ -1,2 +1,14 @@
-hadoop jar chapter3-0-jar-with-dependencies.jar  com.twitter.scalding.Tool hdfsInputTest --hdfs --input hdfs:///dataset/2014/01/01/ --output /result/
-hadoop fs -cat /result/*
+#!/bin/sh
+
+. $(dirname $0)/../hdfs-root-env.sh
+
+# issues with file collisions on case-insensitive file system when exploded locally before shipped
+# to hadoop
+zip -dq target/chapter3-0-jar-with-dependencies.jar META-INF/LICENSE 2>&1 1>/dev/null
+
+hdfs dfs -rm -r -f -skipTrash ${HDFS_ROOT}/result
+
+hadoop jar target/chapter3-0-jar-with-dependencies.jar  com.twitter.scalding.Tool hdfsInputTest \
+    --hdfs --input ${HDFS_ROOT}/dataset/*/*/* --output ${HDFS_ROOT}/result/
+
+hadoop fs -cat ${HDFS_ROOT}/result/*
